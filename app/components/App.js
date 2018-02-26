264e764dbbbbe7332fdcbe3cd6d70ea85a3d3222
markpeou <markpeou@gmail.com>
2018-02-26 15:35:16 +1100

    update react app to get data from express

--- app/components/App.js
+++ app/components/App.js
@@ -5,13 +5,21 @@ export default class App extends React.Component {
 
   constructor(props) {
     super(props)
+    this.state = {info: 'getting from express..'}
   }
+
+componentDidMount() {
+  fetch('https://peaceful-badlands-88229.herokuapp.com/details')
+    .then(res => res.json())
+    .then(res => this.setState({info:res.info}))
+}
 
   render() {
     return <div>
       <h1>hello world</h1>
       <label>example component</label>
       <PasswordInput />
+      <h3>{this.state.info}</h3>
     </div>
   }
 
